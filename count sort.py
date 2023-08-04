154eda2c042d9a7617f6ee0312c59285688030aa
mugilanrs <101979941+mugilanrs@users.noreply.github.com>
2023-08-04 11:03:21 +0530

    Update count sort.py

--- count sort.py
+++ count sort.py
@@ -1,15 +1,14 @@
 def count_sort(arr):
-    # Find the maximum element in the array
+   
     max_element = max(arr)
 
-    # Create a count array to store the occurrences of each element
+  
     count = [0] * (max_element + 1)
 
-    # Count the occurrences of each element in the input array
+  
     for num in arr:  # this create a cummulative sum of the count of the array
         count[num] += 1
 
-    # Reconstruct the sorted array using the count array
     sorted_arr = []
     for i in range(max_element + 1):
         while count[i] > 0:
@@ -18,7 +17,7 @@ def count_sort(arr):
 
     return sorted_arr
 
-# Example usage:
+
 input_array = [4, 2, 2, 8, 3, 3, 1]
 sorted_array = count_sort(input_array)
 print("Sorted Array:", sorted_array)
